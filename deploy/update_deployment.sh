427560cc00f8ce4623517aa524b5dc80287ea275
Eze <hello@ezeanyinabia.com>
2024-08-27 14:00:57 -0400

    Use daemon-reload to update unit file configuration

--- deploy/update_deployment.sh
+++ deploy/update_deployment.sh
@@ -2,7 +2,6 @@
 
 MIX_ENV=prod mix release
 cp deploy/eze_works.service ~/.config/systemd/user/
-systemctl --user stop eze_works.service
-systemctl --user reenable eze_works.service
-systemctl --user start eze_works.service
+systemctl --user daemon-reload
+systemctl --user restart eze_works.service
 systemctl --user status eze_works.service
