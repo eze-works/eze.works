beafdeb6cd46529306f1129402d5052e61f3295b
Eze <hello@ezeanyinabia.com>
2024-08-27 13:59:46 -0400

    Add a commanmd to re-run the release

--- deploy/update_deployment.sh
+++ deploy/update_deployment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+MIX_ENV=prod mix release
 cp deploy/eze_works.service ~/.config/systemd/user/
 systemctl --user stop eze_works.service
 systemctl --user reenable eze_works.service
